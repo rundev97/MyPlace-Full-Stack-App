7b3e51e7f8165290ebc8ca30113873f4330244ab
Eddy <rundev97@gmail.com>
2017-11-04 18:21:34 +0000

    fix css button grid and list

--- public/src/grid.js
+++ public/src/grid.js
@@ -1,6 +1,10 @@
 
 var list = document.querySelector('#list');
 var grid = document.querySelector('#grid');
+grid.style.background = '#aaa';
+grid.style.color = 'white';
+list.style.background = '#ddd';
+list.style.color = '#666';
 
 
 list.addEventListener('click', function(){
@@ -8,6 +12,10 @@ list.addEventListener('click', function(){
         for (var i = 0; i < items.length; i++) {
             items[i].classList.remove('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center' );
             items[i].classList.add('col-md-12', 'list');
+            grid.style.background = '#ddd';
+            grid.style.color = '#666';
+            list.style.background = '#aaa';
+            list.style.color = 'white';
             
         }
 });
@@ -17,6 +25,10 @@ grid.addEventListener('click', function(){
         for (var i = 0; i < items.length; i++) {
             items[i].classList.remove('col-md-12', 'list' );
             items[i].classList.add('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center' );
+            grid.style.background = '#aaa';
+            grid.style.color = 'white';
+            list.style.background = '#ddd';
+            list.style.color = '#666';
         }
     
 });
