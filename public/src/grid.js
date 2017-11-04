971dfcda086e7bc06fa9b600945fe5cf724c15a9
Eddy <rundev97@gmail.com>
2017-11-04 16:18:53 +0000

    fix css list and grid for mobile

--- public/src/grid.js
+++ public/src/grid.js
@@ -4,18 +4,19 @@ var grid = document.querySelector('#grid');
 
 
 list.addEventListener('click', function(){
-    var items = document.querySelectorAll('.col-xl-4, .col-lg-4, .col-md-4, .col-sm-6 ');
+    var items = document.querySelectorAll('.col-xl-4, .col-lg-4, .col-md-4, .col-sm-6 .text-center');
         for (var i = 0; i < items.length; i++) {
-            items[i].classList.remove('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6' );
-            items[i].classList.add('col-sm-12');
+            items[i].classList.remove('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center' );
+            items[i].classList.add('col-md-12', 'list');
+            
         }
 });
 
 grid.addEventListener('click', function(){
-    var items = document.querySelectorAll('.col-sm-12');
+    var items = document.querySelectorAll('.col-md-12');
         for (var i = 0; i < items.length; i++) {
-            items[i].classList.remove('col-sm-12' );
-            items[i].classList.add('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6' );
+            items[i].classList.remove('col-md-12', 'list' );
+            items[i].classList.add('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center' );
         }
     
 });
