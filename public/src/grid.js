4aa5eb3d35bed77cbbef3c01e3d7b6a814905d1e
Eddy <rundev97@gmail.com>
2017-11-05 08:01:00 +0000

    implement pagination display on list page

--- public/src/grid.js
+++ public/src/grid.js
@@ -1,7 +1,7 @@
 
 var list = document.querySelector('#list');
 var grid = document.querySelector('#grid');
-grid.style.background = '#aaa';
+grid.style.background = '#999';
 grid.style.color = 'white';
 list.style.background = '#ddd';
 list.style.color = '#666';
@@ -14,7 +14,7 @@ list.addEventListener('click', function(){
             items[i].classList.add('col-md-12', 'list');
             grid.style.background = '#ddd';
             grid.style.color = '#666';
-            list.style.background = '#aaa';
+            list.style.background = '#999';
             list.style.color = 'white';
             
         }
@@ -25,7 +25,7 @@ grid.addEventListener('click', function(){
         for (var i = 0; i < items.length; i++) {
             items[i].classList.remove('col-md-12', 'list' );
             items[i].classList.add('col-xl-4', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center' );
-            grid.style.background = '#aaa';
+            grid.style.background = '#999';
             grid.style.color = 'white';
             list.style.background = '#ddd';
             list.style.color = '#666';
